eef89e878e1e4c3463190c99ed0e5dc73a1f75e2
Александр Черевко <acherevko@test.ru>
2025-05-25 16:59:16 +0300

    made basic navigation

--- app/src/main/java/com/example/note_takingapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/note_takingapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.note_takingapp
+package com.example.note_takingapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.note_takingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
